1119aa797878612b445b549268ba2ffbebf6a65b
mench0 <mnatsakanyan.armen2014@yandex.ru>
2019-10-20 11:46:38 +0300

    new project

---
+++ tt/src/main/java/main/Frontend.java
@@ -0,0 +1,19 @@
+package main;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.rmi.ServerException;
+
+public class Frontend extends HttpServlet {
+    private String login = "";
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServerException, IOException{
+
+    }
+    public void doPost(HttpServletResponse response, HttpServletRequest request)
+        throws ServerException, IOException{
+
+    }
+}
