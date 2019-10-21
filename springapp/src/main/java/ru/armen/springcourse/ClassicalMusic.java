0ecf7bb1f56544b9d869ca72e8737d642519aeab
mench0 <mnatsakanyan.armen2014@yandex.ru>
2019-10-21 21:56:04 +0300

    anotation

--- springapp/src/main/java/ru/armen/springcourse/ClassicalMusic.java
+++ springapp/src/main/java/ru/armen/springcourse/ClassicalMusic.java
@@ -1,6 +1,10 @@
 package ru.armen.springcourse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+@Component
 public class ClassicalMusic implements Music {
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
--- springapp/src/main/java/ru/armen/springcourse/TestSpring.java
+++ springapp/src/main/java/ru/armen/springcourse/TestSpring.java
@@ -1,14 +1,26 @@
 package ru.armen.springcourse;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.awt.*;
 
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Music music = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-        musicPlayer.playMusic();
+//        Music music = context.getBean("rockMusic", RockMusic.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        musicPlayer.playMusic();
+//
+//        Music classicWeMusic = context.getBean("classicalMusic", ClassicalMusic.class);
+//        MusicPlayer classicalMusicPlayer = new MusicPlayer(classicMusic);
+//        classicalMusicPlayer.playMusic();
+
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
+
         context.close();
 
     }
